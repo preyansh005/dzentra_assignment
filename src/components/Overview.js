fd9287371a78300f589fbc1aab6ba47bb7fd4a5f
Patel Preyansh <patelpreyansh3011@gmail.com>
2022-08-17 23:47:22 +0530

    All Basic CSS Added

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,26 +1,33 @@
 import React from "react";
+import "./Overview.css";
+import pie from "../assets/pie-chart.png"
 
 export default function Overview() {
   return (
-    <div>
+    <div className="overview">
       <h3>Overview</h3>
-      <div>
-        <i className="fa-regular fa-circle"></i>
-        <div>
-          <h5>Pending</h5>
-          <i className="fa-solid fa-circle"></i>
-        </div>
-        <div>
-          <h5>Processing</h5>
-          <i className="fa-solid fa-circle"></i>
-        </div>
-        <div>
-          <h5>Ready To Deliver</h5>
-          <i className="fa-solid fa-circle"></i>
-        </div>
-        <div>
-          <h5>Delivered</h5>
-          <i className="fa-solid fa-circle"></i>
+      <div className="overview-item">
+        <img src={pie} alt="" />
+        <div className="overview-item-1">
+          <div>
+            <i className="fa-solid fa-circle purple"></i>
+            <h5>Pending</h5>
+          </div>
+
+          <div>
+            <i className="fa-solid fa-circle yellow"></i>
+            <h5>Processing</h5>
+          </div>
+
+          <div>
+            <i className="fa-solid fa-circle green"></i>
+            <h5>Ready To Deliver</h5>
+          </div>
+
+          <div>
+            <i className="fa-solid fa-circle blue"></i>
+            <h5>Delivered</h5>
+          </div>
         </div>
       </div>
     </div>
