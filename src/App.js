54ee79d9537f5ebf1de1b9a24962955e42850e6f
Patel Preyansh <patelpreyansh3011@gmail.com>
2022-08-18 00:58:49 +0530

    SideBar Toggle Added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,58 @@
-import './App.css';
-import Delivery from './components/Delivery';
-import Navbar from './components/Navbar';
-import Order from './components/Order';
-import Overview from './components/Overview';
-import SideBar from './components/SideBar';
+import "./App.css";
+import { useState } from "react";
+import Delivery from "./components/Delivery";
+import Navbar from "./components/Navbar";
+import Order from "./components/Order";
+import Overview from "./components/Overview";
+import SideBar from "./components/SideBar";
 
 function App() {
+  const [toggleNavbar, settoggleNavbar] = useState(true);
+  const passData = (data) => {
+    if (data) {
+      settoggleNavbar(false);
+    } else {
+      settoggleNavbar(true);
+    }
+  };
+
   return (
     <>
-    <div className="mainsidebar">
-      <SideBar />
-    </div>
-    <div className="mainsidebar-2">
-      <div className='mainnavbar'>
-        <Navbar />
+      <div className="mainsidebar">{toggleNavbar ? <SideBar /> : <></>}</div>
+
+      <div className="mainsidebar-2">
+        <div className="mainnavbar">
+          <Navbar passData={passData} />
+        </div>
+
+        <div className="orders">
+          <Order
+            name="PENDING ORDER"
+            number={202}
+            className="fa-cart-shopping red"
+          />
+          <Order
+            name="PROCESSING ORDER"
+            number={99}
+            className="fa-cart-shopping yellow"
+          />
+          <Order
+            name="READY TO DELIVER"
+            number={263}
+            className="fa-thumbs-up green"
+          />
+          <Order
+            name="DELIVERED ORDERS"
+            number={345}
+            className="fa-check blue"
+          />
+        </div>
+
+        <div className="section-2">
+          <Delivery />
+          <Overview />
+        </div>
       </div>
-    <div className="orders">
-      <Order name="PENDING ORDER" number={202} className="fa-cart-shopping red"/>
-      <Order name="PROCESSING ORDER" number={99} className="fa-cart-shopping yellow"/>
-      <Order name="READY TO DELIVER" number={263} className="fa-thumbs-up green"/>
-      <Order name="DELIVERED ORDERS" number={345} className="fa-check blue"/>
-    </div>
-    <div className="section-2">
-      <Delivery />
-      <Overview />
-    </div>
-    </div>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,35 @@
-import React from 'react'
-import './Navbar.css'
+import React, { useState } from "react";
+import "./Navbar.css";
 
-export default function Navbar() {
+export default function Navbar(props) {
+  const [toggleSidebar, settoggleSidebar] = useState(true);
+  const toggleSideBar = () => {
+    // console.log("Clicked...")
+    if (toggleSidebar) {
+      // console.log("child true");
+      props.passData(toggleSidebar)
+      settoggleSidebar(false);
+    } else {
+      // console.log("child false");
+      props.passData(toggleSidebar)
+      settoggleSidebar(true);
+    }
+  };
   return (
-    <div className='navbar'>
-        <i className="fa-solid fa-bars"></i>
-        <div className='right-grp'>
-            <div className='right-grp-1'>
-            <i className="fa-solid fa-circle-plus"></i>
-            <i className="fa-solid fa-tag"></i>
-            <i className="fa-solid fa-user"></i>
-            </div>
-
-            <div className='right-grp-2'>
-                <h5>English</h5>
-            <i className="fa-solid fa-angle-down"></i>
-            </div>
+    <div className="navbar">
+      <i onClick={toggleSideBar} className="fa-solid fa-bars"></i>
+      <div className="right-grp">
+        <div className="right-grp-1">
+          <i className="fa-solid fa-circle-plus"></i>
+          <i className="fa-solid fa-tag"></i>
+          <i className="fa-solid fa-user"></i>
         </div>
+
+        <div className="right-grp-2">
+          <h5>English</h5>
+          <i className="fa-solid fa-angle-down"></i>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
